84c543dffd7236843db3b50a93a20f2854ab9bd6
onemx <onemxr@gmail.com>
2012-12-08 14:46:40 +0800

    modify some png file

--- icolletion/icolletion/app/controllers/ViewController.m
+++ icolletion/icolletion/app/controllers/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import "MMLoginViewController.h"
+#import "IIViewDeckController.h"
+
 @interface ViewController ()
 
 @end
@@ -16,7 +18,7 @@
 
 - (void)viewDidLoad
 {
-    self.title = @"Icolletion";
+    [self initView];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -27,9 +29,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) initView{
+    self.title = @"Icolletion";
+    UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    leftButton.frame = CGRectMake(5.0f, 5.0f, 30.0f, 30.0f);
+    [leftButton addTarget:self.viewDeckController action:@selector(toggleLeftView) forControlEvents:UIControlEventTouchUpInside];
+    [leftButton setImage:[UIImage imageNamed:@"nav_launcher.png"] forState:UIControlStateNormal];
+    
+    UIBarButtonItem *leftBarItem = [[UIBarButtonItem alloc]initWithCustomView:leftButton];
+    self.navigationItem.leftBarButtonItem = leftBarItem;
+    self.navigationItem.hidesBackButton = YES;
+
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"nav_bg.png"] forBarMetrics:UIBarMetricsDefault];
+}
+
 - (IBAction)LoginAction:(id)sender {
     MMLoginViewController *mmLoginView = [[MMLoginViewController alloc]initWithNibName:@"MMLoginViewController" bundle:nil];
     [self.navigationController pushViewController:mmLoginView animated:YES];
     [mmLoginView release];
 }
+
+- (void) leftButtonAction:(id)sender{
+    
+}
+
 @end
